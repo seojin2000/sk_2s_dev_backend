0e6add6bfa4c4161e85efa7f5d59bc4b24f10dcc
edu2ucoc <edu2ucoc>
2024-12-19 16:00:08 +0900

    Feat: Java 흐름제어 2-조건문

--- java/JavaBasic/src/Test3.java
+++ java/JavaBasic/src/Test3.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 /**
  * 흐름제어
  *  - 조건문 : if ~ else if ~ else ~
@@ -15,12 +13,22 @@ public class Test3 {
     }
     private static void flowcontrol1() {
         // 1. 콘솔 입력
-        Scanner sc = new Scanner(System.in); // 콘솔에서 사용자 입력 대기하는 코드
-        while(true) {
+        //Scanner sc = new Scanner(System.in); // 콘솔에서 사용자 입력 대기하는 코드
+        //while(true)
+        {
+            System.out.println("Enter number: ");
             // 2. 사용자 입력후 엔터 => 값이 반환
-            int userInputValue = sc.nextInt();
+            int userInputValue = 10;// sc.nextInt(); // 입력 대기
             // 3. 만약 100을 입렷했다면, 종료 혹은 반복문 탈출
-            if (userInputValue == 100) break;
+            //if (userInputValue == 100) break;
+            // 4. 조건식 사용, JS 조건문 동일, 조건이 2개? 3개?
+            if (userInputValue > 10) {
+                System.out.println("10보다 초과한다");
+            }else{
+                System.out.println("10보다 작거나 같다");
+            }
         }
+        // 입력 닫기
+        //sc.close();
     }
 }
