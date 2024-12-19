21c5c8ee6550dc4ea157510dbabe87fc3b10ae84
edu2ucoc <edu2ucoc>
2024-12-19 15:46:10 +0900

    Feat: Java 흐름제어 개요

--- java/JavaBasic/src/Test3.java
+++ java/JavaBasic/src/Test3.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
+/**
+ * 흐름제어
+ *  - 조건문 : if ~ else if ~ else ~
+ *      - 콘솔 입력 사용
+ *  - 반복문 : for ~,  do ~ while, while ~ , continue, break
+ *  - 게임 제작(간단하게)
+ *      - 콘솔 입력 사용
+ */
 public class Test3 {
 }
