21d8b0c8c2880bdd81060e667e7a436753a8accb
edu2ucoc <edu2ucoc>
2024-12-20 16:34:36 +0900

    Feat: Java Enum  기본 사용

--- java/JavaBasic/src/com/example/demo/basic/EnumTest.java
+++ java/JavaBasic/src/com/example/demo/basic/EnumTest.java
@@ -11,4 +11,23 @@ enum Level {
     VVIP, VIP, GOLD, SILVER, BRONZE // 고객 등급
 }
 public class EnumTest {
+    public static void main(String[] args) {
+        // 1. 기본 사용
+        {
+            // 열거형 타입의 변수
+            Level level = Level.VVIP; // 값은 5개중에 한개만 가능
+            // 등급값에 따라 동적으로 처리가 가능한 구조!!
+            switch (level) {
+                case VVIP:
+                    System.out.println("VVIP");
+                    break;
+                case VIP:
+                    System.out.println("VIP");
+                    break;
+                default:
+                    System.out.println("기타 등급");
+                    break;
+            }
+        }
+    }
 }
