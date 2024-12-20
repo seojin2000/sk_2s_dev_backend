2e13b637e789d5afa610852815307b06f525536e
edu2ucoc <edu2ucoc>
2024-12-20 16:21:46 +0900

    Feat: Java 열거형 enum 기본정의

---
+++ java/JavaBasic/src/com/example/demo/basic/EnumTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.basic;
+
+/**
+ * 열거형
+ *  - 상수로 정의되는 여러 변수들을 묶어서 클레스처럼 관리하는 타입
+ *  - 여러 상수를 그룹으로 묶어서 관리!!
+ *  - 내부적인 변수들은 모두 public static final  적용됨
+ */
+// 1. 기본형
+enum Level {
+    VVIP, VIP, GOLD, SILVER, BRONZE // 고객 등급
+}
+public class EnumTest {
+}
