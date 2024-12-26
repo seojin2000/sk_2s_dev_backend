076f4c03fea1ebd1995164d5203da680d1096540
edu2ucoc <edu2.ucoc@gmail.com>
2024-12-26 16:43:09 +0900

    Feat: SB 시큐리티 유저 엔티티 완료

--- springboot/demo_sc/src/main/java/org/example/demo_sc/entity/User.java
+++ springboot/demo_sc/src/main/java/org/example/demo_sc/entity/User.java
@@ -1,9 +1,9 @@
 package org.example.demo_sc.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -15,9 +15,10 @@ import java.util.List;
 @Table(name="userTbl") // 엔티티의 이름과 실제 테이블명을 다르게 하고 싶다면
 @Setter
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본생성자
 @Entity
 // 인증관련 UserDetails을 적용 => 스프링시큐리티 정책
-public class User extends UserDetails {
+public class User implements UserDetails {
     // 단수하게 정리 : Long id,  String email, String password(암호화)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,34 +30,42 @@ public class User extends UserDetails {
     @Column(nullable = false)
     private String password;
 
-    // 6개의 추상 메소드를 추상클레스(UserDetails) 를 상속 받으면서 재정의 해야한다!! -> 룰
+    // 생성자 + 빌더
+    @Builder
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    // 6개의 추상 메소드를 인터페이스(UserDetails) 를 구현 받으면서 재정의 해야한다!! -> 룰
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(new SimpleGrantedAuthority("normal_user")); // 임시 롤 적용
     }
-
     @Override
     public String getUsername() {
-        return "";
+        // 사용자명이 없으므로 email로 대체처리함
+        return email;
     }
-
+    @Override
+    public String getPassword() {
+        // 사용자의 암호화된 암호 대체 처리
+        return password;
+    }
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
-
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
-
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
-
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }
