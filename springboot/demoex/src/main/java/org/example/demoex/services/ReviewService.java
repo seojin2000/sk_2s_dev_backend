f78a242677586edb603ed8a51c2a9cca6005d9d8
edu2ucoc <edu2.ucoc@gmail.com>
2024-12-26 13:54:16 +0900

    Feat: SB 게시판 리뷰 글 가져오기 모듈 구현

--- springboot/demoex/src/main/java/org/example/demoex/services/ReviewService.java
+++ springboot/demoex/src/main/java/org/example/demoex/services/ReviewService.java
@@ -2,11 +2,14 @@ package org.example.demoex.services;
 
 import lombok.RequiredArgsConstructor;
 import org.example.demoex.dto.PostDto;
+import org.example.demoex.dto.ReviewDto;
+import org.example.demoex.entities.Post;
 import org.example.demoex.entities.Review;
 import org.example.demoex.repositories.ReviewRepository;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -26,10 +29,19 @@ public class ReviewService {
         reviewRepository.save(review);
     }
 
-    public void getOneReview(Integer id) {
+    public ReviewDto getOneReview(Integer id) {
         // id -> Review 엔티티
         // 데이터가 존재할때 처리 -> Review 엔티티 -> ReviewDto 반환
-
+        Optional<Review> oReview = this.reviewRepository.findById( id );
+        if( oReview.isPresent() ){ // 리뷰가 존재하면
+            Review review = oReview.get();
+            return ReviewDto.builder()
+                    .id(review.getId())
+                    .content(review.getContent())
+                    .createDate(review.getCreateDate())
+                    .post(review.getPost())
+                    .build();
+        }
         // 없을경우 -> 예외처리를 던지는것으로 정리
         //throw new Exception();
         return null; // 향후 커스텀 예외처리 변경
